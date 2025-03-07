eb32f214bcf2792453f4bf1a3da5cfb5a8426406
MEMBA, Aku <104360361+Rholla999@users.noreply.github.com>
2025-03-07 22:30:53 +0100

    event listeners

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const showBtn = document.getElementById('btn')
 const showMenu = document.querySelector('.nav-links ul')
+const incrementBtn = document.getElementById('addBtn')
+const decrementBtn = document.getElementById('minusBtn')
+const orderCount = document.getElementById('orderCount')
+const amounttoPay = document.getElementById('amount')
+
+const pricePerItem = 125;
 
 showBtn.addEventListener('click', ()=> {
     showMenu.classList.add('active')
@@ -7,4 +13,33 @@ showBtn.addEventListener('click', ()=> {
 
 showMenu.addEventListener('mouseleave', ()=> {
     showMenu.classList.remove('active')
+})
+
+
+incrementBtn.addEventListener('click', ()=> {
+    let currentOrderCount = parseInt(orderCount.value)
+    orderCount.value = currentOrderCount + 1
+   
+    const totalAmount = parseInt(orderCount.value) * pricePerItem;
+    amounttoPay.innerHTML = `$${totalAmount}.00`;
+})
+
+decrementBtn.addEventListener('click', ()=> {
+    if (orderCount.value > 0) {
+        let currentOrderCount = parseInt(orderCount.value)
+        orderCount.value = currentOrderCount - 1
+    }
+
+    const totalAmount = parseInt(orderCount.value) * pricePerItem;
+        amounttoPay.innerHTML = `$${totalAmount}.00`;
+})
+
+
+const allImg = document.querySelectorAll('.images-list img');
+const mainImg = document.getElementById('focusImg');
+
+allImg.forEach((img) => {
+    img.addEventListener('click', ()=> {
+        mainImg.src = img.src ;
+    })
 })
